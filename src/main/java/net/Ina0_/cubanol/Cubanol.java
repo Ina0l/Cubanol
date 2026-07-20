c6247ebec9a5df56177389d1baffde38f0eff58c
Ina0l <hugo75010paris@gmail.com>
2026-07-20 23:48:40 +0200

    unused imports

--- src/main/java/net/Ina0_/cubanol/Cubanol.java
+++ src/main/java/net/Ina0_/cubanol/Cubanol.java
@@ -8,14 +8,11 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Cubanol.MOD_ID)
